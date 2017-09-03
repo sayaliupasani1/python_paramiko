057241a08874f0dc812668516c081ad8e27efb22
sayaliupasani1 <sayaliupasani@gmail.com>
2017-09-03 07:56:53 -0400

    Explicit output

--- sshremote.py
+++ sshremote.py
@@ -2,6 +2,6 @@ import paramiko
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect('127.0.0.1', username='sayaliupasani', password='tvddftt@NOI1')
-output = client.exec_command('ifconfig')
-ifconf = output[1].readlines()
+stdin, stdout, stderr  = client.exec_command('ifconfig')
+ifconf = stdout.readlines()
 print ifconf
